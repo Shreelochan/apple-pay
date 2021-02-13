b1797809f782ba05a72df9d120b8b48c2e68222e
Sumit Mondal <mondal.0508.sumit@gmail.com>
2021-02-14 01:56:32 +0530

    Fixed apple build.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:stripe_payment/stripe_payment.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:stripe_payment/stripe_payment.dart';
 
 String text = 'Click the button to start the payment';
 double totalCost = 10.0;
@@ -14,7 +13,7 @@ double tax = 0.0;
 double taxPercent = 0.2;
 int amount = 0;
 bool showSpinner = false;
-String url ='https://us-central1-demostripe-b9557.cloudfunctions.net/StripePI';
+String url = 'https://us-central1-demostripe-b9557.cloudfunctions.net/StripePI';
 
 void main() {
   runApp(MyApp());
@@ -30,25 +29,28 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     StripePayment.setOptions(
-      StripeOptions(publishableKey: 'pk_test_51IJbpbL7b1ztTOvPVxU0wpWaMWhYlJCbBZ0ywPs2oimywgXv7E7pM6TkkfQoH4DJFTirRjAnjVr29bKcUWGKlzgX00CP9sZlE5',
-        androidPayMode: 'test',),);
+      StripeOptions(
+        publishableKey:
+            'pk_test_51IJbpbL7b1ztTOvPVxU0wpWaMWhYlJCbBZ0ywPs2oimywgXv7E7pM6TkkfQoH4DJFTirRjAnjVr29bKcUWGKlzgX00CP9sZlE5',
+        androidPayMode: 'test',
+        merchantId: 'merchant.tech.wblue.wlive',
+      ),
+    );
   }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Apple Pay'),
-        ),
-        body: FlatButton(
-          onPressed: (){
-            checkIfNativePayReady();
-          },
-          child: Text('Pay'),
-        )
-      ),
+          appBar: AppBar(
+            title: Text('Apple Pay'),
+          ),
+          body: FlatButton(
+            onPressed: () {
+              checkIfNativePayReady();
+            },
+            child: Text('Pay'),
+          )),
     );
-
   }
 
   Future<void> createPaymentMethod() async {
@@ -65,20 +67,21 @@ class _MyAppState extends State<MyApp> {
     }).catchError((e) {
       print('Errore Card: ${e.toString()}');
     });
-    paymentMethod != null ? processPaymentAsDirectCharge(paymentMethod)
+    paymentMethod != null
+        ? processPaymentAsDirectCharge(paymentMethod)
         : showDialog(
-        context: context,
-        builder: (BuildContext context) =>
-            ShowDialogToDismiss(
+            context: context,
+            builder: (BuildContext context) => ShowDialogToDismiss(
                 title: 'Error',
                 content:
-                'It is not possible to pay with this card. Please try again with a different card',
+                    'It is not possible to pay with this card. Please try again with a different card',
                 buttonText: 'CLOSE'));
   }
 
   void checkIfNativePayReady() async {
     print('started to check if native pay ready');
     bool deviceSupportNativePay = await StripePayment.deviceSupportsNativePay();
+    print('DeviceSupportNativePay: $deviceSupportNativePay');
     bool isNativeReady = await StripePayment.canMakeNativePayPayments(
         ['american_express', 'visa', 'maestro', 'master_card']);
     deviceSupportNativePay && isNativeReady
@@ -125,6 +128,7 @@ class _MyAppState extends State<MyApp> {
         items: items,
       ),
     );
+    print('Stripe token: $token');
     paymentMethod = await StripePayment.createPaymentMethod(
       PaymentMethodRequest(
         card: CreditCard(
@@ -132,14 +136,14 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
-    paymentMethod != null ? processPaymentAsDirectCharge(paymentMethod)
+    paymentMethod != null
+        ? processPaymentAsDirectCharge(paymentMethod)
         : showDialog(
-        context: context,
-        builder: (BuildContext context) =>
-            ShowDialogToDismiss(
+            context: context,
+            builder: (BuildContext context) => ShowDialogToDismiss(
                 title: 'Error',
                 content:
-                'It is not possible to pay with this card. Please try again with a different card',
+                    'It is not possible to pay with this card. Please try again with a different card',
                 buttonText: 'CLOSE'));
   }
 
@@ -158,51 +162,48 @@ class _MyAppState extends State<MyApp> {
         StripePayment.completeNativePayRequest();
         setState(() {
           text =
-          'Payment completed. ${paymentIntentX['paymentIntent']['amount'].toString()}p succesfully charged';
+              'Payment completed. ${paymentIntentX['paymentIntent']['amount'].toString()}p succesfully charged';
           showSpinner = false;
         });
-      }else {
+      } else {
         StripePayment.setStripeAccount(strAccount);
         await StripePayment.confirmPaymentIntent(PaymentIntent(
-            paymentMethodId: paymentIntentX['paymentIntent']
-            ['payment_method'],
-            clientSecret: paymentIntentX['paymentIntent']['client_secret']))
-            .then(
-                (PaymentIntentResult paymentIntentResult) async {
-                  final statusFinal = paymentIntentResult.status;
-                  if (statusFinal == 'succeeded') {
-                    StripePayment.completeNativePayRequest();
-                    setState(() {
-                      showSpinner = false;
-                    });
-                  }
-                  else if (statusFinal == 'processing') {
-                    StripePayment.cancelNativePayRequest();
-                    setState(() {
-                      showSpinner = false;
-                    });
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) => ShowDialogToDismiss(
-                            title: 'Warning',
-                            content:
-                            'The payment is still in \'processing\' state. This is unusual. Please contact us',
-                            buttonText: 'CLOSE'));
-                  }
-                  else {
-                    StripePayment.cancelNativePayRequest();
-                    setState(() {
-                      showSpinner = false;
-                    });
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) => ShowDialogToDismiss(
-                            title: 'Error',
-                            content:
-                            'There was an error to confirm the payment. Details: $statusFinal',
-                            buttonText: 'CLOSE'));
-                  }
-                }).catchError((e) {
+                paymentMethodId: paymentIntentX['paymentIntent']
+                    ['payment_method'],
+                clientSecret: paymentIntentX['paymentIntent']['client_secret']))
+            .then((PaymentIntentResult paymentIntentResult) async {
+          final statusFinal = paymentIntentResult.status;
+          if (statusFinal == 'succeeded') {
+            StripePayment.completeNativePayRequest();
+            setState(() {
+              showSpinner = false;
+            });
+          } else if (statusFinal == 'processing') {
+            StripePayment.cancelNativePayRequest();
+            setState(() {
+              showSpinner = false;
+            });
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => ShowDialogToDismiss(
+                    title: 'Warning',
+                    content:
+                        'The payment is still in \'processing\' state. This is unusual. Please contact us',
+                    buttonText: 'CLOSE'));
+          } else {
+            StripePayment.cancelNativePayRequest();
+            setState(() {
+              showSpinner = false;
+            });
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => ShowDialogToDismiss(
+                    title: 'Error',
+                    content:
+                        'There was an error to confirm the payment. Details: $statusFinal',
+                    buttonText: 'CLOSE'));
+          }
+        }).catchError((e) {
           StripePayment.cancelNativePayRequest();
           setState(() {
             showSpinner = false;
@@ -212,13 +213,11 @@ class _MyAppState extends State<MyApp> {
               builder: (BuildContext context) => ShowDialogToDismiss(
                   title: 'Error',
                   content:
-                  'There was an error to confirm the payment. Please try again with another card',
+                      'There was an error to confirm the payment. Please try again with another card',
                   buttonText: 'CLOSE'));
         });
       }
-
-    }
-    else {
+    } else {
       setState(() {
         showSpinner = false;
       });
@@ -227,19 +226,20 @@ class _MyAppState extends State<MyApp> {
           builder: (BuildContext context) => ShowDialogToDismiss(
               title: 'Error',
               content:
-              'There was an error in creating the payment. Please try again with another card',
+                  'There was an error in creating the payment. Please try again with another card',
               buttonText: 'CLOSE'));
     }
   }
 }
 
 class ShowDialogToDismiss extends StatelessWidget {
-
-  
   final String content;
   final String title;
   final String buttonText;
-  ShowDialogToDismiss({this.title, this.buttonText, this.content});  @override
+
+  ShowDialogToDismiss({this.title, this.buttonText, this.content});
+
+  @override
   Widget build(BuildContext context) {
     if (!Platform.isIOS) {
       return AlertDialog(
